c4da04ca00466f7cc84f1dde24a2fcd270af15b9
Muhammad Ibrahim <muhdibee@gmail.com>
2024-04-13 14:30:53 +0100

    Style map.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import 'leaflet/dist/leaflet.css'
+import {MapContainer,TileLayer, Marker, Popup} from 'react-leaflet'
+import L from 'leaflet'
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 function App() {
-
+  // var map = L.map('map').setView([51.505, -0.09], 13);
   return (
     <div className='app-container'>
       <div className='hero-image-container'>
@@ -15,15 +19,25 @@ function App() {
           </div>
         </div>
       </div>
-      <div className='result-container'>
+      <MapContainer
+        center={[9.0563, 7.4985]}
+        zoom={12}>
+        <TileLayer
+          attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* TODO: Add markers */}
+      </MapContainer>
+      
+      {/* <div className='map'>
         <div className='result-info'>
 
         </div>
 
-      </div>
+      </div> */}
       
 
-      <div class="attribution">
+      <div className="attribution">
         Coded by <a href="#">Muhammad Ibrahim</a>. {/*Add my portfolio URL */}
       </div>
     </div>
